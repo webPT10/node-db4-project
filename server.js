9237babcf19255c48eb45a7eb5c2f1e57d79d87e
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-05 12:46:23 -0800

    created welcome/welcome-router.js, imported into server.js, setup

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express")
+const welcomeRouter = require('./welcome/welcome-router')
+// const recipesRouter = require("./recipes/recipes-router")
+// const ingredientsRouter = require("./ingredients/ingredients-router")
+// const quantityRouter = require("./quantity/quantity-router")
 
-const server = express.Router()
+const server = express()
 const port = process.env.PORT || 4040
 
 server.use(express.json())
 
-server.use("/recipes", recipesRouter)
-server.use("/ingredients", ingredientsRouter)
+server.use("/", welcomeRouter)
+// server.use("/recipes", recipesRouter)
+// server.use("/ingredients", ingredientsRouter)
+// server.use("/quantity", quantityRouter)
 
 server.use((err, req, res, next) => {
 	console.log(err)
