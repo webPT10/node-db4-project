a76528ac9e5311a5d3b2b43cb8baaae1c1168cb3
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-05 17:40:31 -0800

    seeds > structured recipes, quantity, ingredients

--- data/seeds/02_quantity.js
+++ data/seeds/02_quantity.js
@@ -1,3 +1,8 @@
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
+    await knex("quantity").insert([
+        { amount: "1 cup"},
+        { amount: "2/3 cup"},
+        {amount: "2 cups"}
+    ])
 };
--- data/seeds/03_ingredients.js
+++ data/seeds/03_ingredients.js
@@ -1,3 +1,9 @@
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
+    await knex("ingredients").insert([
+        { name: "milk", quant_id: 1},
+        { name: "Kix cereal", quant_id: 2},
+        { name: "macaroni", quant_id: 1},
+        { name: "cheddar cheese", quant_id: 3}
+    ])
 };
