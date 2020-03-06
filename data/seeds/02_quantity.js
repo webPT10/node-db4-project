314e3098b6bd24bee6d4d12c510aad360f099df9
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-05 18:14:19 -0800

    created dataSchemes/model.js

--- data/seeds/02_quantity.js
+++ data/seeds/02_quantity.js
@@ -3,6 +3,6 @@ exports.seed = async function(knex) {
     await knex("quantity").insert([
         { amount: "1 cup"},
         { amount: "2/3 cup"},
-        {amount: "2 cups"}
+        { amount: "2 cups"}
     ])
 };
--- data/seeds/04_recipes_ingredients.js
+++ data/seeds/04_recipes_ingredients.js
@@ -1,3 +1,10 @@
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
+    await knex("recipes_ingredients").insert([
+        { recipes_id: 1, ingredients_id: 1 },
+        { recipes_id: 1, ingredients_id: 3 },
+        { recipes_id: 1, ingredients_id: 4 },
+        { recipes_id: 2, ingredients_id: 1 },
+        { recipes_id: 2, ingredients_id: 2 },
+    ])
 };
